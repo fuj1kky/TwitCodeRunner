21301005e1edd139ba8398ad33a50e649a587431
Fujiki Youta <c0nfl1c7.0613@gmail.com>
2019-11-08 03:23:20 +0900

    結果を見やすくした

--- tcr-main.rb
+++ tcr-main.rb
@@ -10,16 +10,15 @@ param = {
   source_code: SOURCE,
   api_key: :guest
 }
-
+#------------------create code------------------
 responce = Net::HTTP.post_form(uri, param)
 create_result = JSON.parse(responce.body)
 cid = create_result["id"]
 
 sleep(2)
 
+#------------------show details------------------
 uri2 = URI.parse("http://api.paiza.io/runners/get_details?id=#{cid}&api_key=guest")
-param2 = { id: cid, api_key: :guest}
 responce = Net::HTTP.get_response(uri2)
-details_result = JSON.parse(responce.body)
-
+details_result = JSON.pretty_generate(JSON.parse(responce.body))
 puts details_result
