cc8b041cc28970c5498ebce41b4079539c55f645
Fujiki Youta <c0nfl1c7.0613@gmail.com>
2019-11-08 02:23:27 +0900

    paiza.ioでコードをコンパイルできた

--- tcr-main.rb
+++ tcr-main.rb
@@ -2,7 +2,8 @@ require_relative '../Privatekey/oauth_twitter'
 require 'json'
 require 'net/http'
 
-SOURCE = "puts 'hello,world!' "
+SOURCE = "puts 'hello,world!' " #test code
+
 uri = URI.parse('http://api.paiza.io/runners/create')
 param = {
   language: :ruby,
@@ -11,5 +12,14 @@ param = {
 }
 
 responce = Net::HTTP.post_form(uri, param)
+create_result = JSON.parse(responce.body)
+cid = create_result["id"]
 
-puts JSON.parse(responce.body)
+sleep(2)
+
+uri2 = URI.parse("http://api.paiza.io/runners/get_details?id=#{cid}&api_key=guest")
+param2 = { id: cid, api_key: :guest}
+responce = Net::HTTP.get_response(uri2)
+details_result = JSON.parse(responce.body)
+
+puts details_result
