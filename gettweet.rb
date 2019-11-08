d2b9ce7af1209aea1fdcd7807529d728582a29d3
Fujiki Youta <c0nfl1c7.0613@gmail.com>
2019-11-08 20:26:04 +0900

    フォロワー（公開アカウントのみ）のツイートの取得

--- gettweet.rb
+++ gettweet.rb
@@ -1,14 +1,33 @@
 require_relative '../Privatekey/oauth_twitter'
 require 'json'
 require 'oauth'
+require 'net/http'
+
+
+id_array = []
 
 consumer = OAuth::Consumer.new(
   @client.consumer_key,
   @client.consumer_secret,
 )
 
-uri = "https://api.twitter.com/1.1/followers/ids.json?screen_name=_fuyok&stringify_ids=true&count=5"
+
+uri = "https://api.twitter.com/1.1/followers/ids.json?screen_name=_fuyok&stringify_ids=true"
 endpoint = OAuth::AccessToken.new(consumer, @client.access_token, @client.access_token_secret)
 responce = endpoint.get(uri)
-result = JSON.pretty_generate(JSON.parse(responce.body))
-puts result
+result = JSON.parse(responce.body)
+
+result["ids"].each do |id|
+  id_array << id
+end
+
+followids = id_array.join(",")
+
+options = {
+  follow: followids
+ }
+
+@client_s.filter(options) do |object|
+  puts object.text
+  puts "---------------------------------------------"
+end
