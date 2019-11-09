091a331f9d3945b106cfa9994082e95e1d8c16fe
Fujiki Youta <c0nfl1c7.0613@gmail.com>
2019-11-10 00:40:13 +0900

    対応言語を拡張

--- apifunc.rb
+++ apifunc.rb
@@ -13,3 +13,24 @@ def show(tweet_id)
 
   return result
 end
+
+def uselang(extention)
+  num = 0
+  language = ""
+  ext_array = ["sh", "c", "cs", "cpp", "clj", "cob", "coffee", "d", "exs", "erl", "fs",
+               "go", "hs", "java", "js", "kt", "sql", "pl", "php", "py", "pyth",
+               "R", "rb", "rs", "scala", "scm", "swift", "vb"]
+
+  lang_array = [:bash, :c, :csharp, :cpp, :clojure, :cobol, :coffeescript, :d,
+                :elixir, :erlang, :fsharp, :go, :haskell, :java, :javascript,
+                :kotlin, :mysql, :perl, :php, :python, :python,
+                :r, :ruby, :rust, :scala, :scheme, :swift, :vb]
+
+  ext_array.each do |ext|
+    if extention == ext
+      language = lang_array[num]
+      return language
+    end
+    num = num + 1
+  end
+end
