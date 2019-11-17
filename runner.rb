348855d8842be1b248f9545a4f06504075103b38
Fujiki Youta <c0nfl1c7.0613@gmail.com>
2019-11-17 21:18:13 +0900

    エラーをリプライできるようにした

--- runner.rb
+++ runner.rb
@@ -24,7 +24,16 @@ def runner(source,language, screen_name)
     uri2 = URI.parse("http://api.paiza.io/runners/get_details?id=#{cid}&api_key=guest")
     responce = Net::HTTP.get_response(uri2)
     details_result = JSON.parse(responce.body)
-    text = "@#{screen_name}\n" + details_result["stdout"]
+    puts JSON.pretty_generate(details_result)
+    if details_result["result"] == "success"
+      text = "@#{screen_name}\n" + details_result["stdout"]
+    elsif details_result["result"] == "failure"
+      text = "@#{screen_name}\n" + details_result["stderr"]
+    elsif details_result["result"].nil?
+      text = "@#{screen_name}\n" + details_result["build_stderr"]
+    else
+      text = "@#{screen_name}\n" + "何らかのエラーが発生しました。"
+    end
     @client.update text
   end
 end
